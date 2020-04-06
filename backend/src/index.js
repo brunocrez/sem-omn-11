7a922b86ca06da9475df92cd198d913f1d2f6b42
Bruno Rezende <brunoc.rez@gmail.com>
2020-04-06 10:47:22 -0300

    frontend for be the hero

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000, () => {
-    console.log('Server is UP on Port 3000!')
+app.listen(3333, () => {
+    console.log('Server is UP on Port 3333!')
 })
